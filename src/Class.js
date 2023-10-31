69df6242bcd432bdb658d2c899d15505361598a3
rushikesh <rushikeshsobale@gmail.com>
2023-10-31 17:48:57 +0530

    sdsdsddssd

--- src/Class.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Class extends Component {
-  render() {
-    return (
-      <div>Class</div>
-    )
-  }
-}
--- src/icons.js
+++
@@ -1,8 +0,0 @@
-// icons.js
-
-import { library } from '@fortawesome/fontawesome-svg-core';
-import {faCircleChevronDown, faCoffee, faAddressCard, faEnvelope } from '@fortawesome/free-solid-svg-icons';
-
-library.add(faCircleChevronDown,faCoffee, faAddressCard, faEnvelope);
-
-export {faCircleChevronDown, faCoffee, faAddressCard, faEnvelope };
